a04c3f182571ac64d1d933468be6d770a0ab2d9e
Albert Chang <aznachang@gmail.com>
2017-05-22 13:55:38 -0700

    Update RestParamSpreadOperator.js

--- RestParamSpreadOperator.js
+++ RestParamSpreadOperator.js
@@ -10,22 +10,3 @@ let multiply = (i, ...nums) => {
 };
 let result = multiply(2, 5, 10, 15);
 console.log(result);
-
-
-Golda James, Senior Software Developer 12:00-1:00 pm
-
-Jeff Decker, Staff Engineer,
-
-Gautam Mankatti, Product Development 1:00-2:00 pm
-
-Shrenik Chougule, Senior Software Developer
-
-Break 2:00-2:15 pm
-
-Rao Vemula, Technical Recruiter 2:15-2:30 pm
-
-Ravindra Satyanarayan, Manager, QA  2:30-3:45 pm
-
-Payal Parikh, Senior Manager, Development  3:15-4:00 pm
-
-Srilatha Davuluri, Product Manager
